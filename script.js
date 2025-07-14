04989b3654040457799a891b594bedc43dfee711
fatpepol <markusbjorgeziegler@hotmail.com>
2025-07-14 22:58:18 +0200

    hmtl structure and js data gather

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 async function getData() {
-  try {
     const response = await fetch(
       'https://pokeapi.co/api/v2/pokemon/masquerain'
     );
+    
     const data = await response.json();
-    console.log(data);
     return data;
-  } catch (error) {
-    console.error(`${error}: cockshitterton`);
-  }
 }
 
 async function displayData() {
   const data = await getData();
-  console.log(data);
+  data.stats.forEach(element => {
+    console.log(element.stat.name + " " +element.base_stat );
+  });
 }
+
+displayData();
